53332f673bf84417e870773b80819a881cab94d4
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-24 11:31:06 +0300

    Add functionality to lights

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", lights={"r": False, "y": False, "g": True})
 
 if __name__ == "__main__":
     app.run()
