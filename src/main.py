e50133dc72f4d85051faf88505ec98216ab22954
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-24 11:42:03 +0300

    Add read & write

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
 from fetch import get_todays_prices
+
+def write(prices:dict):
+    with open("todays_prices.csv", "w") as file:
+        for timestamp, price in prices.items():
+            row = f"{timestamp};{price}"
+            file.write(row + "\n")
+
+def read():
+    data = {}
+    with open("todays_prices.csv", "r") as file:
+        for row in file:
+            timestamp, price = row.replace("\n", "").split(";")
+            data[timestamp] = price
+    return data
